a5e90fcc3b0ccd6e531292d99ad3c3e2e2b53b12
alecs <aferrarini01@gmail.com>
2020-07-28 15:18:43 +0200

    added rofi

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TARGET="$HOME"/.config
+
+ln -s "$ABSOLUTE_PATH"/alacritty $TARGET
+ln -s "$ABSOLUTE_PATH"/bspwm     $TARGET
+ln -s "$ABSOLUTE_PATH"/dunst     $TARGET
+ln -s "$ABSOLUTE_PATH"/picom     $TARGET
+ln -s "$ABSOLUTE_PATH"/polybar   $TARGET
+ln -s "$ABSOLUTE_PATH"/scripts   $TARGET
+ln -s "$ABSOLUTE_PATH"/sxhkd     $TARGET
+ln -s "$ABSOLUTE_PATH"/xob       $TARGET
+ln -s "$ABSOLUTE_PATH"/rofi      $TARGET
+ln -s "$ABSOLUTE_PATH"/secrets   $TARGET
