e3ff71230103fbda17d2e4c23e5b2c0c31946762
alecs <aferrarini01@gmail.com>
2020-09-02 16:46:48 +0200

    minor wacom

--- scripts/wacom.sh
+++ scripts/wacom.sh
@@ -6,13 +6,13 @@ pad="$(echo "$list" | grep PAD | cut -f 2 | sed "s/id: //")"
 
 case ${1:-"xournal"} in
     xournal)
-        xsetwacom set $stylus Button 2 "key +ctrl +shift P -ctrl -shift"
-        xsetwacom set $stylus Button 3 "key +ctrl +shift E -ctrl -shift"
-        xsetwacom set $stylus PressureCurve 60 100 0 40
-        xsetwacom set $pad Button 8 "key +ctrl - -ctrl"
-        xsetwacom set $pad Button 9 "key +ctrl + -ctrl"
-        xsetwacom set $pad Button 3 "key up"
-        xsetwacom set $pad Button 1 "key down"
+        xsetwacom set "$stylus" Button 2 "key +ctrl +shift P -ctrl -shift"
+        xsetwacom set "$stylus" Button 3 "key +ctrl +shift E -ctrl -shift"
+        xsetwacom set "$stylus" PressureCurve 60 100 0 40
+        xsetwacom set "$pad" Button 8 "key +ctrl - -ctrl"
+        xsetwacom set "$pad" Button 9 "key +ctrl + -ctrl"
+        xsetwacom set "$pad" Button 3 "key up"
+        xsetwacom set "$pad" Button 1 "key down"
         echo "Mode xournal is set"
         ;;
     *)
