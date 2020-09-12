0505914015c7cee1fb2084ffb3903bec54cff4af
alecs <aferrarini01@gmail.com>
2020-09-12 17:54:12 +0200

    monitor minor

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,8 +4,8 @@ extern=HDMI-1
 
 if xrandr | grep "$extern disconnected"; then
     echo single
-    xrandr --output "$extern" --off --output "$intern" --auto
-    bspc monitor -d 1 2 3 4 5 6 7 8 9 10
+    xrandr --output "$intern" --auto
+    bspc monitor $intern -d 1 2 3 4 5 6 7 8 9 10
 else
     echo multi
     xrandr --output "$extern" --left-of "$intern" --auto --primary
