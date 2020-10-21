d96a4d8ad17edded4fccf2a0d47fbbd6cf3180f0
alecs <aferrarini01@gmail.com>
2020-10-21 16:30:10 +0200

    dnd mode

---
+++ scripts/dnd.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+TOGGLE_FILE="$HOME/.cache/dnd_toggle_file"
+
+if [ ! -e $TOGGLE_FILE ]; then
+	touch $TOGGLE_FILE
+	notify-send.py "Entering DnD mode" \
+				   "From now on all notifications will be deactivated" \
+				   -a "DnD manager" \
+				   -i sleep
+	notify-send.py a --hint boolean:deadd-notification-center:true \
+					 string:type:pausePopups
+else
+	rm $TOGGLE_FILE
+	notify-send.py a --hint boolean:deadd-notification-center:true \
+					 string:type:unpausePopups
+	notify-send.py "Deactivatig DnD mode" \
+				   "From now on all notifications will be displayed" \
+				   -a "DnD manager" \
+				   -i sleep
+fi
