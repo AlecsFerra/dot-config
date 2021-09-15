ce5bf22072fb20d6ec4be0257f3ad1590e71cda3
alecs <alecsferra@protonmail.com>
2021-09-15 15:15:59 +0200

    Added dead keys to us keyboard

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -4,10 +4,12 @@ if lsusb -v 2>/dev/null \
    | grep "Microdia USB Keyboard" 1>/dev/null
 then
     setxkbmap -option caps:swapescape \
+              -option lv3:ralt_switch_multikey \
               -option grp:alt_shift_toggle us,it &
     echo us
 else
     setxkbmap -option caps:swapescape \
+              -option lv3:ralt_switch_multikey \
               -option grp:alt_shift_toggle it,us &
     echo it
 fi
