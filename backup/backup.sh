f2c56b6b569398231229584e93aef9961ccfe493
alecsferra <alecsferra@protonmail.com>
2021-12-31 17:01:45 +0100

    Fixed bkp dir

--- backup/backup.sh
+++ backup/backup.sh
@@ -3,11 +3,10 @@
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 home="/home/alecs/"
-ackuplocation="/media/BackupDisk/backupArch"
+backuplocation="/media/BackupDisk/backupArch"
 
 rsync -arvzp \
   --progress \
   --exclude-from="$dir/exclude.txt" \
   --files-from="$dir/include.txt" \
-  "$home" \
-  "$backuplocation"
+  "$home" "$backuplocation"
