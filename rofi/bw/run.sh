9961cc7aa43c1a0d9b5237641384485395457128
alecsferra <alecsferra@protonmail.com>
2022-07-18 22:01:36 +0200

    bwmenuv1

--- rofi/bw/run.sh
+++ rofi/bw/run.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
-
-bwmenu \
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/bwmenu \
   -c 10 \
-  --auto-lock 1800 \
-  -- -config "$HOME/.config/rofi/bw/theme.rasi" \
-  -dmenu \
-  -no-fixed-num-lines
+  --auto-lock 1800 
